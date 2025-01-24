17ffb5ae1e1a42698a0211c4abfa8ae673e0ab97
matheus-from-irev <matheus.m@irev.com>
2025-01-24 18:02:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("send-request-button").addEventListener("click", function() {
 
     const clickId = new URLSearchParams(window.location.search).get("click_id");
-    var postback = "https://demo-igaming.irev.com/backend/api/v3/goal-type-fire/14?hash=REPLACE"
+    var postback = "https://demo-igaming.irev.com/backend/api/v3/goal-type-fire/15?hash=REPLACE"
     postback = postback.replace("REPLACE", clickId);
     fetch(postback)
     console.log(postback)
